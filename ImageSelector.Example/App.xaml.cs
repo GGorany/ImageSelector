1dc18e3067151b9eeb3d88843b0d448a4a79b325
jung1768 <jung1768@hanmail.net>
2021-08-12 18:05:51 +0900

    초기 커밋.

---
+++ ImageSelector.Example/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+
+using System.Threading;
+using System.Windows;
+
+using ImageSelector.Example.Views;
+
+namespace ImageSelector.Example
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        private static Mutex _mutex = null;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string appName = "ImageSelector.Example";
+            bool createdNew;
+
+            _mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application  
+                Application.Current.Shutdown();
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ ImageSelector.Example/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace ImageSelector.Example.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        #region Properties
+        private string _FileName = string.Empty;
+
+        public string FileName
+        {
+            get { return _FileName; }
+            set { SetProperty(ref _FileName, value); }
+        }
+        #endregion
+
+        #region Commands
+        private DelegateCommand _OpenFile;
+
+        public DelegateCommand OpenFile => _OpenFile ??= new DelegateCommand(OnOpenFile);
+        #endregion
+
+        #region Constructor
+        public MainWindowViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Private Method
+        private void OnOpenFile()
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.DefaultExt = ".jpg";
+            dialog.Filter = "image files (.jpg)|*.jpg";
+
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+                this.FileName = dialog.FileName;
+        }
+        #endregion
+    }
+}
