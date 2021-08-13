2374751a797d5eac03070b2e2362d6d71b975e6c
jung1768 <jung1768@hanmail.net>
2021-08-13 16:43:57 +0900

    Second Commit.

--- ImageSelector.Example/ViewModels/MainWindowViewModel.cs
+++ ImageSelector.Example/ViewModels/MainWindowViewModel.cs
@@ -1,35 +1,42 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 
+using System;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+
 namespace ImageSelector.Example.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         #region Properties
         private string _FileName = string.Empty;
+        private BitmapSource _SourceImage = null;
+        private Rectangle _Rect;
 
         public string FileName
         {
             get { return _FileName; }
             set { SetProperty(ref _FileName, value); }
         }
+
+        public BitmapSource SourceImage
+        {
+            get { return _SourceImage; }
+            set { SetProperty(ref _SourceImage, value); }
+        }
+
+        public Rectangle Rect
+        {
+            get { return _Rect; }
+            set { SetProperty(ref _Rect, value); }
+        }
         #endregion
 
         #region Commands
         private DelegateCommand _OpenFile;
 
-        public DelegateCommand OpenFile => _OpenFile ??= new DelegateCommand(OnOpenFile);
-        #endregion
-
-        #region Constructor
-        public MainWindowViewModel()
-        {
-
-        }
-        #endregion
-
-        #region Private Method
-        private void OnOpenFile()
+        public DelegateCommand OpenFile => _OpenFile ??= new DelegateCommand(() => 
         {
             var dialog = new Microsoft.Win32.OpenFileDialog();
             dialog.DefaultExt = ".jpg";
@@ -37,8 +44,22 @@ namespace ImageSelector.Example.ViewModels
 
             bool? result = dialog.ShowDialog();
             if (result == true)
+            {
                 this.FileName = dialog.FileName;
+                this.SourceImage = new BitmapImage(new Uri(this.FileName));
+            }
+        });
+        #endregion
+
+        #region Constructor
+        public MainWindowViewModel()
+        {
+            this.Rect = new Rectangle(0, 0, 0, 0);
         }
+        #endregion
+
+        #region Private Method
+
         #endregion
     }
 }
---
+++ ImageSelector/Rectangle/Rectangler.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Drawing;
+
+namespace ImageSelector
+{
+    /// <summary>
+    /// ROI.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Rectangler : UserControl
+    {
+        private double magnification = 0.0;
+
+        #region Properties
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(
+            "Source",
+            typeof(ImageSource),
+            typeof(Rectangler),
+            new PropertyMetadata(default(ImageSource), OnSourceChanged));
+
+        public static readonly DependencyProperty RectProperty = DependencyProperty.Register(
+            "Rect",
+            typeof(Rectangle),
+            typeof(Rectangler),
+            new PropertyMetadata(default(Rectangle), OnRectChanged));
+
+        public ImageSource Source
+        {
+            get { return (ImageSource)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public Rectangle Rect
+        {
+            get { return (Rectangle)GetValue(RectProperty); }
+            set { SetValue(RectProperty, value); }
+        }
+        #endregion
+
+        public Rectangler()
+        {
+            InitializeComponent();
+
+            SourceImage.LayoutTransform = new ScaleTransform();
+
+            MouseHandler.MouseWheel += MouseHandler_MouseWheel;
+            MouseHandler.MouseMove += MouseHandler_MouseMove;
+            MouseHandler.MouseLeftButtonDown += MouseHandler_MouseLeftButtonDown;
+            MouseHandler.MouseLeftButtonUp += MouseHandler_MouseLeftButtonUp;
+        }
+
+        #region Events
+        private void MouseHandler_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double zoom_delta = e.Delta > 0 ? .1 : -.1;
+            magnification = (magnification += zoom_delta).LimitToRange(.1, 10);
+            //CenterViewerAroundMouse(MousePosition);
+            this.ShowMousePosition();
+            e.Handled = true;
+        }
+
+        private void MouseHandler_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.ShowMousePosition();
+        }
+
+        private void MouseHandler_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void MouseHandler_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Private Method
+        private void ShowMousePosition()
+        {
+            var pos = Mouse.GetPosition(this.SourceImage);
+            if ((pos.X >= 0) && (pos.X < this.SourceImage.Width) && (pos.Y >= 0) && (pos.Y < this.SourceImage.Height))
+            {
+                this.TextBox_MousePosition.Text = $"x = {Mouse.GetPosition(this.SourceImage).X:N0}; y = {Mouse.GetPosition(this.SourceImage).Y:N0}";
+            }
+        }
+
+        private static void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is Rectangler rectangler)) return;
+            if (e.NewValue is ImageSource newImage)
+            {
+                rectangler.SourceImage.Source = newImage;
+                rectangler.SourceImage.Width = newImage.Width;
+                rectangler.SourceImage.Height = newImage.Height;
+                //cropper.CanvasPanel.UpdateLayout();
+                //cropper.AdornerCrop(cropper.Rect);
+            }
+            else
+            {
+                rectangler.SourceImage.Source = null;
+                rectangler.SourceImage.Width = double.NaN;
+                rectangler.SourceImage.Height = double.NaN;
+                //cropper.CanvasPanel.UpdateLayout();
+                //cropper.CroppingAdorner.Crop = Rectangle.Empty;
+            }
+        }
+
+        private static void OnRectChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is Rectangler rectangler)) return;
+            //if (cropper.CroppingAdorner == default) return;
+            //if (e.NewValue is Rectangle rect)
+            //{
+            //    cropper.AdornerCrop(rect);
+            //}
+            //else
+            //{
+            //    cropper.CroppingAdorner.Crop = Rectangle.Empty;
+            //}
+        }
+
+        private void CanvasPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            //if (sender is FrameworkElement visual)
+            //{
+            //    AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(visual);
+            //    if (adornerLayer == null)
+            //        return;
+
+            //    CroppingAdorner = new CroppingAdorner(visual);
+            //    adornerLayer.Add(CroppingAdorner);
+            //    AdornerCrop(Rect);
+            //    CroppingAdorner.OnRectangleSizeEvent += CroppingAdorner_OnRectangleSizeEvent;
+            //}
+        }
+
+        #endregion
+
+    }
+}
